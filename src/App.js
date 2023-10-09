fd6173bf2490fa0525f2ccb039ef387aab34a0ba
SHAIK FARIDHUDEEN <shaiknijas7@gmail.com>
2023-10-09 18:19:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,56 +1,25 @@
-import React, { useState, useEffect } from "react";
+import logo from './logo.svg';
+import './App.css';
 
-import MovieCard from "./MovieCard";
-import SearchIcon from "./search.svg";
-import "./App.css";
-
-const API_URL = "http://www.omdbapi.com?apikey=b6003d8a";
-
-const App = () => {
-    const [searchTerm, setSearchTerm] = useState("");
-    const [movies, setMovies] = useState([]);
-
-    useEffect(() => {
-        searchMovies("Batman");
-    }, []);
-
-    const searchMovies = async (title) => {
-        const response = await fetch(`${API_URL}&s=${title}`);
-        const data = await response.json();
-
-        setMovies(data.Search);
-    };
-
-    return (
-        <div className="app">
-            <h1>CIN3 POP </h1>
-
-            <div className="search">
-                <input
-                    value={searchTerm}
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                    placeholder="Search for movies"
-                />
-                <img
-                    src={SearchIcon}
-                    alt="search"
-                    onClick={() => searchMovies(searchTerm)}
-                />
-            </div>
-
-            {movies?.length > 0 ? (
-                <div className="container">
-                    {movies.map((movie) => (
-                        <MovieCard movie={movie} />
-                    ))}
-                </div>
-            ) : (
-                <div className="empty">
-                    <h2>No movies found</h2>
-                </div>
-            )}
-        </div>
-    );
-};
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
 export default App;
